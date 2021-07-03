4071c7381b914fee4d632a17255c40a19f97397b
zuofei <zuofei.chou@tuya.com>
2021-07-03 15:08:46 +0800

    feat. update proxy and env

--- account-app/src/setupProxy.js
+++ account-app/src/setupProxy.js
@@ -4,9 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'https://iot-app-smart-office-cn.wgine-inc.com:7799', //预发环境
-      // target: 'https://iot-app-smart-office.wgine-daily.com:7799', //日常环境
-      target: 'http://localhost:8001', //本地mock
+      target: 'http://localhost:8080', //本地
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',
--- main-app/src/setupProxy.js
+++ main-app/src/setupProxy.js
@@ -4,10 +4,9 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'https://iot-app-smart-office-cn.tuya-inc.com:7799', // 生产
-      // target: 'https://iot-app-smart-office-cn.wgine-inc.com:7799', //预发环境
-      target: 'https://iot-app-smart-office.wgine-daily.com:7799', //日常环境
-      // target: 'http://localhost:8001',
+      target: 'http://iot-app-smart-office.wgine-daily.com:7799', //日常环境
+
+      // target: 'https://iot-portal.fast-daily.tuya-inc.cn',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',
@@ -18,11 +17,11 @@ module.exports = function (app) {
   app.use(
     '/asset-app',
     createProxyMiddleware({
-      target: 'http://localhost', // 本地静态
-      // target: 'http://localhost:7001', // 本地开发
-      // pathRewrite: { // 本地开发
-      //   '^/asset-app': '',
-      // },
+      // target: 'http://localhost', // 本地静态
+      target: 'http://localhost:7001', // 本地开发
+      pathRewrite: { // 本地开发
+        '^/asset-app': '',
+      },
       changeOrigin: true,
     }),
   );
@@ -30,11 +29,11 @@ module.exports = function (app) {
   app.use(
     '/device-app',
     createProxyMiddleware({
-      target: 'http://localhost', // 本地静态
-      // target: '//localhost:7002', // 本地开发
-      // pathRewrite: { // 本地开发
-      //   '^/device-app': '',
-      // },
+      // target: 'http://localhost', // 本地静态
+      target: '//localhost:7002', // 本地开发
+      pathRewrite: { // 本地开发
+        '^/device-app': '',
+      },
       changeOrigin: true,
     }),
   );
